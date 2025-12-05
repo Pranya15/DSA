96554b8f8e2db3a564d95bb4be8e1968ac54aeb9
Pranya15 <patelpranya15@gmail.com>
2025-12-05 22:58:52 +0530

    Added createnode file only

---
+++ createnode.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    //constructor function 
+    Node(int value){
+       this-> data= value;
+       this-> next= nullptr;
+    }
+};
+int main(){
+    int value;
+    cout<< "Enter the value to be inserted in the first node:";
+    cin>> value;
+    Node *Node1= new Node(value);
+    Node *head = Node1;
+    cout<< Node1 -> data <<endl;
+    cout<< Node1-> next;
+    return 0;
+}
