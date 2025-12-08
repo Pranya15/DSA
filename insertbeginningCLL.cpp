a961985719a0f0c56c28b64a16843b0a66f663de
Pranya15 <patelpranya15@gmail.com>
2025-12-08 08:55:20 +0530

    Added insertbeginningCLL

---
+++ insertbeginningCLL.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int data;
+	Node *next;
+};
+
+Node *head = NULL;
+
+void insertAtBegCLL() {
+	int newData;
+	cout << "\nEnter the value: ";
+	cin >> newData;
+
+	// Create a new node
+	Node *newNode = new Node();
+	newNode->data = newData;
+
+	// If the list is empty
+	if (head == NULL) {
+		newNode->next = newNode;
+		head = newNode;
+	}
+	else {
+		Node *temp = head;
+
+		// Traverse to the last node
+		while (temp->next != head) {
+			temp = temp->next;
+		}
+
+		newNode->next = head;     // link newNode to current head
+		temp->next = newNode;     // link last node to newNode
+		head = newNode;           // move head to newNode
+	}
+}
+
+int main() {
+
+	int n;
+	cout << "How many nodes to insert at beginning? ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		insertAtBegCLL();
+	}
+
+	return 0;
+}
