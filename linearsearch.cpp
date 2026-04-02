c4dda5125a02f77a7b77801f5db8b6db78e05ed1
Pranya15 <patelpranya15@gmail.com>
2026-04-02 11:01:09 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+    int linearSearch(int arr[], int sz, int target){
+        for(int i=0; i<sz; i++){
+            if(arr[i]== target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+int main (){
+    int arr[] = {4,5,9,8,6,1};
+    int sz = 6;
+    int target = 6;
+    cout << linearSearch(arr,sz,target) << endl;
+    return 0;
+}
