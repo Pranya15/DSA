813571b90ad25e5ee3500919d576d8dd92ebf679
Pranya Patel <patelpranya15@gmail.com>
2026-04-13 12:32:14 +0530

    added

---
+++ countdigit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n) {
+    if (n == 0) return 0;   
+
+    return 1 + countDigits(n / 10);
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    cout << "Digits = " << countDigits(n);
+
+    return 0;
+}
